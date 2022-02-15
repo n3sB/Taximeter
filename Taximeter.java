f1e0488361f821855427cfdc688ebf75b829ec58
N3sB <enesbaysl@outlook.com>
2022-02-15 23:39:51 +0300

    Taximeter Calculator

---
+++ Taximeter.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Taximeter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int km, openingFee = 10;
+        double totalCost, perKm = 2.20;
+        
+
+        System.out.print("Please enter the distance:(km) ");
+        km = scanner.nextInt();
+        scanner.close();
+
+        totalCost = openingFee + (km*perKm);
+
+        totalCost = (totalCost < 20) ? 20 : totalCost;
+
+
+        System.out.println("Total cost is " + totalCost);
+
+    }
+}
